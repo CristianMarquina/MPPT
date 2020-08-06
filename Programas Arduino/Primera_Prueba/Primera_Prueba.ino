80f5cf09c6cbe290c0590ecc22f449e05f81e85f
15-10872@usb.ve <15-10872@usb.ve>
2020-08-05 20:30:05 -0400

    Ya leo el voltaje y la corriente del arreglo

--- Programas Arduino/Primera_Prueba/Primera_Prueba.ino
+++ Programas Arduino/Primera_Prueba/Primera_Prueba.ino
@@ -1,31 +1,48 @@
 // These constants won't change. They're used to give names to the pins used:
 const float analogVpvIn = A0;  // Analog input pin that the potentiometer is attached to
-const float analogOutPin = A5; // Analog output pin that the LED is attached to
+const float analogIpvPin = A1; // Analog output pin that the LED is attached to
 
-float sensorValue = 0;        // value read from the pot
-float outputValue = 0;      // value output to the PWM (analog out)
-float voltage = 0;
+float Vpvin = 0;        // Lectura del voltaje del arrglo
+float Vpv= 0;
+float sensorValue=0;
+
+float Ipvin = 0;        //Lectura de la corriente del arreglo
+float Ipv= 0;
+
+float VCC= 5;// Voltaje de alimentacion del sensor de corriente
+float  sensitivity =0.100;// sensibilidad del ACS712ELCTR-20A-T
+const float QOV =   0.5 * VCC;// set quiescent Output voltage of 0.5V
+float voltage;// internal variable for voltage
+
+
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the analog in value:
+  // Lectura de voltaje
   sensorValue = analogRead(analogVpvIn);
-  // map it to the range of the analog out:
- outputValue = 10.0/3.0;
-  // change the analog out value:
-  voltage =  (sensorValue*5.0)/1023.0;
-  analogWrite(analogOutPin, outputValue);
+  Vpv=  (sensorValue*5.0*42.0)/1023.0;
 
+
+  
+  float voltage_raw =   (5.0 / 1023.0)* analogRead(analogIpvPin);// Read the voltage from sensor
+  voltage =  voltage_raw - QOV + 0.012 ;// 0.000 is a value to make voltage zero when there is no current
+  float current = voltage / sensitivity;
+ 
+
+  // change the analog out value:
+  
   // print the results to the Serial Monitor:
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.print("\t output = ");
-  Serial.print(outputValue);
-  Serial.print("\t vol = ");
-  Serial.print(voltage);
+  Serial.print("Vpv = ");
+  Serial.print(Vpv);
+  Serial.println("V");
+
+   
+  Serial.print("\t Ipv: ");
+  Serial.print(current,2); // print the current with 2 decimal places
+  Serial.println("A");
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
